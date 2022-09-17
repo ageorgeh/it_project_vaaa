4832728eddc75d0c262224506ea62c0cd422abf6
aashimajaiswal <aashimamaths@gmail.com>
2022-09-17 20:24:37 +1000

    Create API: add new book

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { db } = require('./firebase.js')
 
 const port = process.env.PORT || 3001;
 // Enable cors security headers
@@ -9,6 +10,18 @@ app.use(cors())
 // add an express method to parse the POST method
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+// CREATE: add a new book
+app.post('/MyBooks/AddNewBook', async (req, res) => {
+  const { currUID, newTitle } = req.body
+  const randomString = (Math.random() + 1).toString(36).substring(7);
+  const newBookRef = db.collection('books').doc(randomString)
+  const res2 = await newBookRef.set({
+      uid: currUID,
+      title: newTitle
+  }, { merge: true })
+  res.status(200).send('Added new book ' + newTitle + ' for user ' + currUID )
+})
 
 // home page
 app.get('/', (req, res) => {
