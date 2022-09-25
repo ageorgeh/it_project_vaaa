b2eb57efdd9aa69b8572503a20fe195e1043bc2e
akitonakamura2 <82019432+akitonakamura2@users.noreply.github.com>
2022-09-25 21:12:09 +1000

    eslint fixes
    eslint fixes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 const {db} = require('./models/firebase.js')
 
-const port = process.env.PORT || 3001;
+
+const port = process.env.PORT || 3001
 // Enable cors security headers
 app.use(cors())
 
 // add an express method to parse the POST method
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // MyBooks page routes
 // CREATE: add a new book
@@ -57,14 +58,14 @@ app.post('/MyBooks/DeleteBook', async (req, res) => {
   const bookRef = db.collection('books').doc(bookID).delete();
   res.status(200).send('Deleted book with bookID ' + bookID )
 // test books
-app.get("/api", (req, res) => {
-  res.json({ "books": ["book 1", "book 2", "book 3"] })
+app.get('/api', (req, res) => {
+  res.json({ books: ['book 1', 'book 2', 'book 3'] })
 })
 
 // home page
 app.get('/', (req, res) => {
   res.send('Hi There')
-});
+})
 
 // get all of the books in the database
 app.get('/get', (req, res) => {
@@ -72,18 +73,18 @@ app.get('/get', (req, res) => {
 })
 
 // add a book to the database
-app.post("/insert", (req, res) => {
+app.post('/insert', (req, res) => {
   res.send('post')
 })
 
 // delete a book from the database
-app.delete("/delete/:bookId", (req, res) => {
+app.delete('/delete/:bookId', (req, res) => {
   res.send('delete')
 })
 
 // update a book review
-app.put("/update/:bookId", (req, res) => {
+app.put('/update/:bookId', (req, res) => {
   res.send('put')
 })
 
-app.listen(port, () => {console.log("Server listening on", port)})
+app.listen(port, () => { console.log('Server listening on', port) })
