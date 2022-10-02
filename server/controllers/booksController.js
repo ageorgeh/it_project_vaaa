9f10ff41f5cbcfa8a10c070d37a6022c12942b38
aashimajaiswal <aashimamaths@gmail.com>
2022-10-02 16:01:38 +1100

    added controllers to follow MVC

---
+++ server/controllers/booksController.js
@@ -0,0 +1,54 @@
+const {db} = require('../models/firebase')
+
+// CREATE: add a new book
+const AddNewBook = async function(req, res) {
+    const {currUID, title} = req.body;
+    const bookID = (Math.random() + 1).toString(36).substring(7);
+    const newBookRef = db.collection('books').doc(bookID)
+    const res2 = await newBookRef.set({
+        bookID : bookID,
+        uid: currUID,
+        title: title
+    }, { merge: true })
+    res.status(200).send('Added new book ' + title + ' for user ' + currUID )
+}
+
+// READ: get books data by user id
+const getUserBooks = async function(req, res) {
+    const { currUID} = req.body
+    const booksRef = db.collection('books')
+    const booksRes = await booksRef.get()
+    let books = [];
+    booksRes.forEach(doc => {
+      books.push(doc.data());
+    });
+    // filter on currUID
+    const booksByUID = books.filter(book => book.uid == currUID)
+    res.send(booksByUID);
+}
+
+// UPDATE: change a book's title
+const updateTitle = async function(req, res) {
+    const { bookID,currUID, newTitle } = req.body
+    const bookRef = db.collection('books').doc(bookID)
+    const res2 = await bookRef.set({
+        bookID: bookID,
+        currUID: currUID,
+        title: newTitle
+    }, { merge: true })
+  res.status(200).send('Updated title to' + newTitle + ' for bookID ' + bookID )
+}
+
+// DELETE: deletes a book
+const deleteBook = async function(req, res) {
+    const {bookID} = req.body
+    const bookRef = db.collection('books').doc(bookID).delete();
+    res.status(200).send('Deleted book with bookID ' + bookID )
+}
+
+module.exports = {
+    AddNewBook,
+    getUserBooks,
+    updateTitle,
+    deleteBook
+}
