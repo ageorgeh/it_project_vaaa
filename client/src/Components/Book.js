5d56f66643b2f3014d2dc0cb08efcb20c5ed8d88
Vincentnilam <vincent.nilam@hotmail.com>
2022-10-06 11:58:52 +1100

    Add edit button when hovering over book

--- client/src/Components/Book.js
+++ client/src/Components/Book.js
@@ -16,9 +16,10 @@ class Book extends React.Component {
             <div className="group relative w-1/6 h-64 place-content-center mb-1">
                 <div className="h-48 w-32 flex justify-center items-center overflow-hidden relative">
                     <img src={this.props.image} className="shrink-0 min-h-full min-w-full"alt="book cover" />
-                    <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-3xl text-white font-semibold">Edit</div>
+                    
                     {/* hover background */}
                     <div className="absolute bg-black w-full h-full opacity-0 transition-opacity duration-500 group-hover:opacity-80" />
+                    <button type="button" className="absolute invisible group-hover:visible text-gray-100 bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 m-2">Edit</button>
                 </div>
                 <div className="w-32">
                     <p className="text-stone-100 text-sm break-words line-clamp-2">{this.props.title}</p>
