14910243478b20a088550a2a3cbebbb82d05b16f
bigtiger67-stack <aghornung@gmail.com>
2022-10-15 00:16:29 +1100

    Just changed test so that it will work

--- client/src/Components/Login.test.js
+++ client/src/Components/Login.test.js
@@ -1,20 +1,18 @@
-import {render, screen} from '@testing-library/react'
-import Login from './Login';
+import { render, screen } from '@testing-library/react'
+import Login from './Login'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
+test('should render Login component', () => {
+  useAuthState.mockReturnValue([false, false])
 
-test('should render Login component', () =>{
-
-    useAuthState.mockReturnValue([false, false])
-
-    render(
+  render(
         <BrowserRouter>
         <Login />
         </BrowserRouter>
-      )
-    expect(screen.getByPlaceholderText('e-mail')).not.toBeNull()
-    expect(screen.getByPlaceholderText('password')).not.toBeNull()
-    expect(screen.getByText("Sign in with Google")).not.toBeNull()
+  )
+  expect(screen.getByPlaceholderText('Email')).not.toBeNull()
+  expect(screen.getByPlaceholderText('Password')).not.toBeNull()
+  expect(screen.getByText('Sign in with Google')).not.toBeNull()
 })
