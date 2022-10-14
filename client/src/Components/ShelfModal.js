94b7ff748d8e2ea93b285272aba1d11981478b5f
bigtiger67-stack <aghornung@gmail.com>
2022-10-14 13:07:51 +1100

    Update ShelfModal.js

--- client/src/Components/ShelfModal.js
+++ client/src/Components/ShelfModal.js
@@ -135,7 +135,7 @@ export default function Modal ({ visible, onClose, fieldValues, shelves }) {
   return (
         <div id='modalContainer'
         onClick ={handleOnClose}
-        className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm flex justify-center items-center">
+        className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-sm z-10 flex justify-center items-center">
             <div className ="bg-white p-5 rounded">
                 <p className="text-center mb-5">{fieldValues ? 'Update shelf' : 'Add shelf'}</p>
                 <form onSubmit={fieldValues ? submitUpdate : submitChanges}>
