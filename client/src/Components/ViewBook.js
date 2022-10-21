877b375a60bacf488968f0e60ae96a2068571e8d
henryhamer <103616397+henryhamer@users.noreply.github.com>
2022-10-21 16:18:55 +1100

    added a back button in viewbooks

--- client/src/Components/ViewBook.js
+++ client/src/Components/ViewBook.js
@@ -109,6 +109,14 @@ function ViewBook () {
               </div>
           </div>
 
+          <div className="mt-8 flex justify-center">
+            <button className="mb-2 text-font bg-buttonNeutral font-medium rounded-lg text-sm px-5 py-2.5">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+              </svg>
+              </button>
+          </div>
+
           </>
 
     )
